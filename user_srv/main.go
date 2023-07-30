1d3adc4e72dd7ebd5eeb718cfa19261ef822d84d
Piwriw <Piwriw@163.com>
2023-07-30 22:34:07 +0800

    [feat]:add user-srv consul 服注册
    Signed-off-by: Piwriw <Piwriw@163.com>

--- user_srv/main.go
+++ user_srv/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/hashicorp/consul/api"
+	"github.com/nacos-group/nacos-sdk-go/inner/uuid"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
@@ -41,7 +43,41 @@ func main() {
 	}
 	//注册服务健康检查
 	grpc_health_v1.RegisterHealthServer(server, health.NewServer())
+	//服务注册
+	cfg := api.DefaultConfig()
+	cfg.Address = fmt.Sprintf("%s:%d", global.AppConf.Consul.IP,
+		global.AppConf.Consul.Port)
 
+	client, err := api.NewClient(cfg)
+	if err != nil {
+		panic(err)
+	}
+	//生成对应的检查对象
+	check := &api.AgentServiceCheck{
+		GRPC:                           fmt.Sprintf("%s:%d", global.AppConf.Grpc.IP, global.AppConf.Grpc.Port),
+		Timeout:                        "5s",
+		Interval:                       "5s",
+		DeregisterCriticalServiceAfter: "15s",
+	}
+
+	//生成注册对象
+	registration := new(api.AgentServiceRegistration)
+	registration.Name = global.AppConf.Grpc.Name
+	u, err := uuid.NewV4()
+	if err != nil {
+		panic("failed new  uuid:" + err.Error())
+	}
+	serviceID := fmt.Sprintf("%s", u.String())
+	registration.ID = serviceID
+	registration.Port = global.AppConf.Grpc.Port
+	registration.Tags = global.AppConf.Grpc.Tags
+	registration.Address = global.AppConf.Grpc.IP
+	registration.Check = check
+
+	err = client.Agent().ServiceRegister(registration)
+	if err != nil {
+		panic(err)
+	}
 	go func() {
 		err = server.Serve(lis)
 		if err != nil {
@@ -53,7 +89,7 @@ func main() {
 	quit := make(chan os.Signal)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
-	if err = global.Client.Agent().ServiceDeregister("servcieid"); err != nil {
+	if err = global.Client.Agent().ServiceDeregister(serviceID); err != nil {
 		zap.S().Info("注销失败")
 	}
 	zap.S().Info("注销成功")
